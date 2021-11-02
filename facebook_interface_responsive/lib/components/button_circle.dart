40948e9655b6bdc808d456056e3d63c41b1194e6
Junior Safera <juniorsafera.dev@gmail.com>
2021-11-02 12:15:33 -0300

    Update Buttons_Circle app bar

--- facebook_interface_responsive/lib/components/button_circle.dart
+++ facebook_interface_responsive/lib/components/button_circle.dart
@@ -3,33 +3,40 @@ import 'package:line_icons/line_icons.dart';
 
 class ButtonCircle extends StatelessWidget {
 
+  // Variáveis 
   final IconData icone;
   final double iconeTamanho;
   final VoidCallback onPressed;
 
 
   const ButtonCircle({ Key? key,
+      // Construtores
       required this.icone,
       required this.iconeTamanho,
       required this.onPressed,
+
    }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+              // Personalizando o container
               decoration: BoxDecoration(
                 color: Colors.grey[200],
                 shape: BoxShape.circle 
                 ),
 
-              margin: EdgeInsets.all(5),
-
+                // Propriedades do Button
                 child: IconButton(
                   onPressed: (){},
                   icon: Icon(icone),
                   iconSize: iconeTamanho,              
                   color: Colors.black,
                 ),
+                
+                // margem que separa o container
+                 margin: EdgeInsets.all(5),
+
             );
   }
 }
