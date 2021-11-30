4ab477a35f5cfea7e7e78dc5609c311b8a8e1dde
Junior Safera <juniorsafera.dev@gmail.com>
2021-11-30 17:44:02 -0300

    criando_navegação_entre_abas

---
+++ facebook_interface_responsive/lib/views/principal.dart
@@ -0,0 +1,45 @@
+import 'package:facebook_interface_responsive/components/nav_abas.dart';
+import 'package:facebook_interface_responsive/views/home.dart';
+import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+
+class Principal extends StatefulWidget {
+  const Principal({ Key? key }) : super(key: key);
+
+  @override
+  _PrincipalState createState() => _PrincipalState();
+}
+
+class _PrincipalState extends State<Principal> {
+
+  List<Widget> _telas = [
+    Home(),
+    Scaffold(backgroundColor: Colors.green),
+    Scaffold(backgroundColor: Colors.amberAccent),
+    Scaffold(backgroundColor: Colors.red),
+    Scaffold(backgroundColor: Colors.purple),
+  ];
+
+
+  List<IconData> _icones = [
+    Icons.home,
+    Icons.ondemand_video,
+    Icons.storefront_outlined,
+    Icons.flag_outlined,
+    LineIcons.bell,
+    Icons.menu
+  ];
+
+  int indexAbaSelected = 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  DefaultTabController(
+      length: _icones.length, 
+      child: Scaffold(
+        body: TabBarView(children: _telas),
+        bottomNavigationBar: NavegacaoAbas() ,) 
+      );
+  }
+}
