d4b3aae3b119eb5be9fe6232f01fe397b0dd595b
Junior Safera <juniorsafera.dev@gmail.com>
2021-12-14 17:28:03 -0300

    update_responsividade

--- facebook_interface_responsive/lib/others/responsive.dart
+++ facebook_interface_responsive/lib/others/responsive.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 
 class Responsivo extends StatelessWidget {
+
+  final Widget mobile;
+  final Widget? tablet;
+  final Widget desktop;
  
-  const Responsivo({ Key? key }) : super(key: key);
+  const Responsivo({ Key? key,
+      required this.mobile,
+      this.tablet,
+      required this.desktop,
+
+   }) : super(key: key);
 
   static bool isMobile(BuildContext context){
     return MediaQuery.of(context).size.width < 800;
@@ -22,7 +32,20 @@ class Responsivo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return LayoutBuilder(
+      builder: (context, constraints){
+            if(constraints.maxWidth >= 1200){
+              return desktop;
+            } else if(constraints.maxWidth >= 800 ){
+              Widget? resTablet = this.tablet;
+              if(resTablet != null){
+              return resTablet;
+              } 
+              return this.mobile;
+            } else{
+              return mobile;
+            }
+      },
       
     );
   }
--- facebook_interface_responsive/lib/views/home.dart
+++ facebook_interface_responsive/lib/views/home.dart
@@ -5,6 +5,7 @@ import 'package:facebook_interface_responsive/components/create_post.dart';
 import 'package:facebook_interface_responsive/data/data.dart';
 import 'package:facebook_interface_responsive/models/posts.dart';
 import 'package:facebook_interface_responsive/others/palette_colors.dart';
+import 'package:facebook_interface_responsive/others/responsive.dart';
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
 
@@ -19,7 +20,22 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
+      body: Responsivo(
+        mobile: HomeMobile(),
+        tablet: HomeTablet(),
+        desktop: HomeDesktop(),
+      ),
+    );
+  }
+}
+
+
+class HomeMobile extends StatelessWidget {
+  const HomeMobile({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
         slivers: [
 
           // Área do AppBar
@@ -89,7 +105,133 @@ class _HomeState extends State<Home> {
              ),
              ),
         ],
-      ),
-    );
+      );
+  }
+}
+
+
+class HomeTablet extends StatelessWidget {
+  const HomeTablet({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+        slivers: [
+
+          // Área do AppBar
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            //expandedHeight: 250,
+            floating: true,
+            centerTitle: false,
+
+            title: Text('facebook', 
+            style: TextStyle(
+              color: Palette_Colors.azulFacebook,
+              fontWeight: FontWeight.bold,
+              fontSize: 28,
+              letterSpacing: -1.2,
+           ),
+           ),
+           actions: [
+
+            ButtonCircle(
+              icone: Icons.search, 
+              iconeTamanho: 30, 
+              onPressed: (){}
+            ),
+
+            ButtonCircle(
+              icone: LineIcons.facebookMessenger, 
+              iconeTamanho: 30, 
+              onPressed: (){}
+            ),
+
+           ],
+          ),
+
+
+          // Área de criação de posts
+          SliverToBoxAdapter(
+             child: CreatePost(usuario: usuarioAtual,) , 
+          ),
+
+           
+
+           // Área de exibição de storys
+           SliverPadding(
+             padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+             sliver: SliverToBoxAdapter(
+                  child: AreaStory(
+                    usuario: usuarioAtual,
+                    storys: storys,
+                  ) ,
+             ),
+             ),
+     
+
+             
+          // Área de exibição de posts
+           SliverList(
+             delegate: SliverChildBuilderDelegate(
+               (context, index){
+                  Post post = posts[index];
+
+                  return CartaoPost(
+                    post: post
+                    );
+               },
+               childCount: posts.length,
+             ),
+             ),
+        ],
+      );
+  }
+}
+
+
+class HomeDesktop extends StatelessWidget {
+  const HomeDesktop({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+        slivers: [
+
+           
+
+
+          // Área de exibição de storys
+           SliverPadding(
+             padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+             sliver: SliverToBoxAdapter(
+                  child: AreaStory(
+                    usuario: usuarioAtual,
+                    storys: storys,
+                  ) ,
+             ),
+             ),
+
+          // Área de criação de posts
+          SliverToBoxAdapter(
+             child: CreatePost(usuario: usuarioAtual,) , 
+          ),
+
+            
+          // Área de exibição de posts
+           SliverList(
+             delegate: SliverChildBuilderDelegate(
+               (context, index){
+                  Post post = posts[index];
+
+                  return CartaoPost(
+                    post: post
+                    );
+               },
+               childCount: posts.length,
+             ),
+             ),
+        ],
+      );
   }
 }
